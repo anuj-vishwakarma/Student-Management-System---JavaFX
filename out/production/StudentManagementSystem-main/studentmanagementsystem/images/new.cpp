2dcfed9fc8cc8a99b7c72b90054a327dd283defe
Anuj Vishwakarma <160238331+anuj-vishwakarma@users.noreply.github.com>
2025-07-19 16:20:30 +0530

    Add files via upload

---
+++ out/production/StudentManagementSystem-main/studentmanagementsystem/images/new.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+char getUserChoice();
+char getComputerChoice();
+
+int main (){
+    cout << "Rock, Paper, and Scissors game" << endl;
+    cout << "Enter\n'r' for Rock \n" << "'p' for Paper \n" << "'s' for scissor" << endl;
+ 
+    char choice ;
+    cin >> choice;
+
+    
+    return 0;
+}
+char getComputerChoice (){
+
+}
+char getUserChoice (){
+
+}
